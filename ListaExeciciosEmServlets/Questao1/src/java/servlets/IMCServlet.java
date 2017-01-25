b9dab169ba7175cb7409e6b9b0e9432cc4d9cff2
MANOEL <MANOELNETOM@GMAIL.COM>
2017-01-25 19:28:11 -0300

    Reposta da Questão 1 da Lista em Servlets

---
+++ ListaExeciciosEmServlets/Questao1/src/java/servlets/IMCServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import negocio.Usuario;
+
+/**
+ *
+ * @author ManoelNeto
+ */
+@WebServlet(name = "IMCServlet", urlPatterns = {"/IMCServlet"})
+public class IMCServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Usuario usuario = new Usuario();
+            usuario.setPeso(Double.parseDouble(request.getParameter("peso")));
+            usuario.setAltura(Double.parseDouble(request.getParameter("altura")));
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet IMCServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            String botao = request.getParameter("bt");
+            if (botao.equalsIgnoreCase("IMC Tradicional")) {
+                out.println("<h1> " + usuario.getIMCTradicional() + "</h1>");
+            } else if (botao.equalsIgnoreCase("IMC Hildenburg")) {
+                out.println("<h1> " + usuario.getIMCHildenburg() + "</h1>");
+            } else {
+                out.println("<h1> " + usuario.getIMCOxford() + "</h1>");
+
+            }
+
+            out.println("<a href=\"index.html\">Clique aqui para Voltar</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
