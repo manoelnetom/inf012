13e171c91851ce52744cff8096b3b0049958171a
MANOEL <MANOELNETOM@GMAIL.COM>
2017-01-30 21:12:24 -0300

    Reposta da Questão 3 Jogo Zero a Cem em Servlet

---
+++ ListaExeciciosEmServlets/Questao3/src/java/controle/ZeroCemServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import negocio.Jogo;
+
+/**
+ *
+ * @author ManoelNeto
+ */
+@WebServlet(name = "ZeroCemServlet", urlPatterns = {"/ZeroCemServlet"})
+public class ZeroCemServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession ses =request.getSession();
+            Jogo jogo =(Jogo)ses.getAttribute("jogo");
+            if(jogo==null){
+                jogo=new Jogo();
+                ses.setAttribute("jogo", jogo);
+            }
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Jogo Zero a Cem em Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ZeroCemServlet at " + jogo.getResposta() + "</h1>");
+            out.println("<form action=\"ZeroCemServlet\" method=\"POST\">\n" +
+"            Número: <input type=\"text\" name=\"numero\"/>\n" +
+"            <input type=\"submit\" value=\"Jogar\"/>\n" +
+"        </form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ ListaExeciciosEmServlets/Questao3/src/java/negocio/Jogo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ManoelNeto
+ */
+public class Jogo {
+    private int numero;
+    private int sorteado;
+    private int tentaivas;
+
+    public Jogo() {
+        this.sortear();
+        
+    }
+    
+    private void sortear(){
+        this.tentaivas=0;
+        Random r=new Random();
+        this.sorteado=r.nextInt(101);
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getSorteado() {
+        return sorteado;
+    }
+    
+   public String getResposta(){
+       this.tentaivas++;
+       String resposta="";
+       if(this.getNumero()>this.getSorteado()){
+           resposta="O número que vc digitou é maior que o sorteado";
+       }else if (this.getNumero()<this.getSorteado()){
+           resposta="O número que vc digitou é menor que o sorteado";
+       }else{
+           resposta="Parabéns, você acertou depois de "+this.tentaivas +"! O número sorteado é "+this.getSorteado();
+           this.sortear();
+       }
+       return resposta;
+   } 
+    
+    
+}
