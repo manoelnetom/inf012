b46092ce2c57aab17793d2a2fa8cd099eb38678d
manoel <manoelnetom@gmail.com>
2021-03-23 16:37:32 -0300

    Novos exemplos

---
+++ javascript/reactapps/biscoitodasorte/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import biscoito from './assets/biscoito.png';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.frases = ['A vida trará coisas boas se tiveres paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Não há que ser forte.'
+      ];
+      this.state = {
+        fraseSorteada:''
+      };
+
+  }
+    abrirBiscoito(){
+      let numero = Math.floor(Math.random()*this.frases.length);
+      this.setState({fraseSorteada:this.frases[numero]});
+    }
+
+  render() {
+    return (
+      <div className="conteiner">
+        <img alt="biscoito" src={biscoito} className="img"/>
+        <button onClick={this.abrirBiscoito.bind(this)}>Sortear</button>
+        <h1 className="textoFrase">{this.state.fraseSorteada}</h1>
+      </div>
+    );
+  }
+}
+export default App;
--- javascript/reactapps/formularioimc/src/components/IMC/index.js
+++ javascript/reactapps/formularioimc/src/components/IMC/index.js
@@ -4,11 +4,21 @@ class IMC extends Component{
     constructor(props){
         super(props);
         this.state = {
+            form:{
             peso:0,
             altura:0,
             imc:""
+            }
         }
+        this.dadosForm=this.dadosForm.bind(this);
     }
+
+    dadosForm(e){
+        let state =this.state;
+        state.form[e.target.name]=e.target.value;
+        this.setState(state);
+    }
+
     calcular(){
         let state= this.state;
         let imc=state.peso/state.altura**2;
@@ -18,8 +28,8 @@ class IMC extends Component{
     render(){
         return(
         <div>
-            Peso: <input type="text" value={this.state.peso} onChange={(e)=> this.setState({peso:e.target.value})}/>
-            Altura: <input type="text" value={this.state.altura} onChange={(e)=> this.setState({altura:e.target.value})}/>
+            Peso: <input type="text" name="peso" value={this.state.peso} onChange={(e)=> this.setState({peso:e.target.value})}/>
+            Altura: <input type="text" name="altura" value={this.state.altura} onChange={(e)=> this.setState({altura:e.target.value})}/>
             <button onClick={this.calcular.bind(this)}>Calcular</button>
             <br/>
             <h1>{this.state.imc}</h1>
